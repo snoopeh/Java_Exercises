5f7f987692273ff1d5545a0974ad028c09c59177
Aluno <Aluno@w03266582.cipucrs.pucrs.br>
2019-04-24 15:37:16 -0300

    Correção de arrayList para array.

--- ExerciciosAula1/src/Exercício1.java
+++ ExerciciosAula1/src/Exercício1.java
@@ -21,37 +21,51 @@ public class Exercício1 {
 		System.out.println("Lista de Números Repetidos");
 		List<Integer> repeatArray = listRepeat(ar);
 		for (int i = 0; i < repeatArray.size(); i++) {
-			System.out.println(repeatArray.get(i));
+			System.out.print(repeatArray.get(i) + " ");
 		}
-		System.out.println("======================");
+		System.out.println("\n======================");
+		System.out.println("Array 1: ");
+		for (int i = 0; i < ar.length; i++) {
+			System.out.print(ar[i] + " ");
+		}
+		System.out.println("\n======================");
+		System.out.println("Array 2: ");
+		for (int i = 0; i < ar2.length; i++) {
+			System.out.print(ar2[i] + " ");
+		}
+		System.out.println("\n======================");
 		System.out.println("Array União: ");
 
-		List<Integer> uArray = union(ar, ar2);
-		System.out.println("Tamanho da lista é: " + uArray.size());
-		for (int i = 0; i < uArray.size(); i++) {
-			System.out.println(uArray.get(i));
+		int[] uArray = union(ar, ar2);
+		System.out.println("Tamanho da lista é: " + uArray.length);
+		for (int i = 0; i < uArray.length; i++) {
+			System.out.print(uArray[i] + " ");
 		}
-		System.out.println("======================");
 
-		List<Integer> intArray = intersection(ar, ar2);
+		System.out.println("\n======================");
+
+		int[] intArray = intersection(ar, ar2);
 		System.out.println("Elementos de intersecção: ");
-		for (int i = 0; i < intArray.size(); i++) {
-			System.out.println(intArray.get(i));
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.print(intArray[i] + " ");
 		}
-		System.out.println("======================");
-
+		System.out.println("\n======================");
 	}
-
-	private static List<Integer> intersection(int[] ar, int[] ar2) {
-		List<Integer> l3 = new ArrayList<Integer>();
-		for (int j = 0; j <= ar.length - 1; j++) {
-			if (verificaIntersec(ar, ar2[j])) {
-				if (!l3.contains(ar2[j])) {
-					l3.add(ar2[j]);
-				}
+	
+	private static int[] intersection(int[] l1, int[]l2) {
+		int[] l3 = new int[l1.length + l2.length];
+		int count = 0;
+		for (int j = 0; j <= l1.length - 1; j++) {
+			if (verificaIntersec(l2, l1[j]) && !verificaIntersec(l3, l1[j])) {
+				l3[count] = l1[j];
+				count++;
 			}
 		}
-		return l3;
+		int[] l4 = new int[count];
+		for (int i = 0; i <= count - 1; i++) {
+			l4[i] = l3[i];
+		}
+		return l4;
 	}
 
 	private static boolean verificaIntersec(int[] ar, int i) {
@@ -66,25 +80,32 @@ public class Exercício1 {
 	}
 
 	// Método responsável pela lista de união
-	private static List<Integer> union(int[] l1, int[] l2) {
-		List<Integer> l3 = new ArrayList<Integer>();
+	private static int[] union(int[] l1, int[] l2) {
+		int[] l3 = new int[l1.length + l2.length];
+		int count = 0;
 		for (int j = 0; j <= l1.length - 1; j++) {
 			if (verificaUniao(l3, l1[j])) {
-				l3.add(l1[j]);
+				l3[count] = l1[j];
+				count++;
 			}
 			if (verificaUniao(l3, l2[j])) {
-				l3.add(l2[j]);
+				l3[count] = l2[j];
+				count++;
 			}
 		}
-		return l3;
+		int[] l4 = new int[count];
+		for (int i = 0; i <= count - 1; i++) {
+			l4[i] = l3[i];
+		}
+		return l4;
 	}
 
 	// Método responsável por verificar se os elementos da lista de união não são
 	// repetidos
-	private static boolean verificaUniao(List<Integer> a1, int el) {
+	private static boolean verificaUniao(int[] a1, int el) {
 		int i = 0;
-		while (i <= a1.size() - 1) {
-			if (a1.get(i) == el) {
+		while (i <= a1.length - 1) {
+			if (a1[i] == el) {
 				return false;
 			}
 			i++;
@@ -133,9 +154,9 @@ public class Exercício1 {
 	// Método responsável pela criação de arrays para os exercícios
 	private static int[] criaArray() {
 		Random rand = new Random();
-		int[] array1 = new int[20];
+		int[] array1 = new int[6];
 		for (int i = 0; i < array1.length; i++) {
-			array1[i] = rand.nextInt(30);
+			array1[i] = rand.nextInt(4);
 		}
 		return array1;
 	}
